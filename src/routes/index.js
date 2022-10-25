da703555089dd2f11d82429dcf018ffe7c2827da
KaosYaiba <wandson.lopes.706@ufrn.edu.br>
2022-10-25 11:53:47 -0300

    jj

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Welcome from '../pages/Welcome'
-import SignIn from '../pages/SignIn'
+import Welcome from '../pages/Welcome';
+import SignIn from '../pages/SignIn';
 import { StackActions } from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
